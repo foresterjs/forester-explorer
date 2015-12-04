ab37f7c62a40b9562574ca1b423949db10429ca9
Christian Vadalà <chr.vadala@gmail.com>
2015-12-04 18:39:09 +0100

    bump version

--- build/index.js
+++ build/index.js
@@ -7,43 +7,59 @@ exports.serveSchema = serveSchema;
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } step("next"); }); }; }
 
-const router = require('koa-simple-router');
-const path = require('path');
+var router = require('koa-simple-router');
+var path = require('path');
 
-module.exports = function (forester) {
+module.exports = function (config) {
 
-  forester.registerStaticRoute({ route: '/explorer', path: path.join(__dirname, '../public') });
+  return function (forester) {
 
-  forester.koa.use(router(function (_) {
-    _.get('/schema', serveSchema({ collections: forester.collections, rest: forester.rest }));
-  }));
+    forester.registerStaticRoute({ route: '/explorer', path: path.join(__dirname, '../public') });
+
+    forester.koa.use(router(function (_) {
+      _.get('/schema', serveSchema({ collections: forester.collections, rest: forester.rest }));
+    }));
+  };
 };
 
 function serveSchema(_ref) {
-  let collections = _ref.collections;
-  let rest = _ref.rest;
+  var collections = _ref.collections;
+  var rest = _ref.rest;
 
   return (function () {
-    var ref = _asyncToGenerator(function* (_ref2, next) {
-      let request = _ref2.request;
-      let response = _ref2.response;
+    var ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee(_ref2, next) {
+      var request = _ref2.request;
+      var response = _ref2.response;
+      var endpointsDescription, collectionName, collectionSchema;
+      return regeneratorRuntime.wrap(function _callee$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              endpointsDescription = [];
 
-      var endpointsDescription = [];
+              for (collectionName in collections) {
+                collectionSchema = collections[collectionName].schema;
 
-      for (var collectionName in collections) {
-        var collectionSchema = collections[collectionName].schema;
-        endpointsDescription.push({
-          name: collectionName,
-          properties: collectionSchema.properties,
-          relations: collectionSchema.relations,
-          endpoints: rest.routes[collectionName] || []
-        });
-      }
+                endpointsDescription.push({
+                  name: collectionName,
+                  properties: collectionSchema.properties,
+                  relations: collectionSchema.relations,
+                  endpoints: rest.routes[collectionName] || []
+                });
+              }
 
-      response.body = endpointsDescription;
+              response.body = endpointsDescription;
 
-      yield next;
-    });
+              _context.next = 5;
+              return next;
+
+            case 5:
+            case 'end':
+              return _context.stop();
+          }
+        }
+      }, _callee, this);
+    }));
 
     return function (_x, _x2) {
       return ref.apply(this, arguments);
